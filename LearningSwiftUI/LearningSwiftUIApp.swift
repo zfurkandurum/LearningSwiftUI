8a22fbee476bbc9cc309143c03f5eb22fc89b27a
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2023-09-30 13:13:02 +0300

    Initial Commit

---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Zeki Furkan Durum on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
